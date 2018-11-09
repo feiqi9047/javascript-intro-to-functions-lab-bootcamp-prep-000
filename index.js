7c65264a99bbd42d859ccea3bc5f0c0400dd6e5e
Fei Qi <feiqi9047@gmail.com>
2018-11-09 08:14:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function whisper(string) {
 'Hello!'.toLowerCase()
 
 function logShout(string) {
-  console.log(string).toUpperCase(string)
+  console.log(string.toUpperCase)
 }
 'Hello!'.toUpperCase()
